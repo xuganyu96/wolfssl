c0854e242c03556e0105400a2284497752aed15c
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-05-14 10:38:24 -0400

    preliminary port of HQC to wolfcrypt API

---
+++ wolfcrypt/src/pqclean_hqc.c
@@ -0,0 +1,333 @@
+#include <wolfssl/wolfcrypt/settings.h>
+
+#include <wolfssl/wolfcrypt/error-crypt.h>
+#include <wolfssl/wolfcrypt/logging.h>
+#include <wolfssl/wolfcrypt/pqclean_hqc.h>
+#include <wolfssl/wolfcrypt/random.h>
+#include <wolfssl/wolfcrypt/types.h>
+
+WOLFSSL_API int wc_PQCleanHqcKey_Init(PQCleanHqcKey *key) {
+    return wc_PQCleanHqcKey_InitEx(key, NULL, INVALID_DEVID);
+}
+
+/* public and private keys are stack allocated, no need to use heap */
+WOLFSSL_API int wc_PQCleanHqcKey_InitEx(PQCleanHqcKey *key, void *heap, int devId) {
+    if (key == NULL) {
+        return BAD_FUNC_ARG;
+    }
+    XMEMSET(key, 0, sizeof(PQCleanHqcKey));
+    return 0;
+}
+
+/* Clear all data from key. Similar to Init, but this function will never fail
+ */
+WOLFSSL_API int wc_PQCleanHqcKey_Free(PQCleanHqcKey *key) {
+    if (key != NULL) {
+        XMEMSET(key, 0, sizeof(PQCleanHqcKey));
+    }
+    return 0;
+}
+
+/* Return 1 if level is valid */
+static int is_valid_level(int level) { return (level == 1) || (level == 3) || (level == 5); }
+
+WOLFSSL_API int wc_PQCleanHqcKey_SetLevel(PQCleanHqcKey *key, int level) {
+    if ((key == NULL) || !is_valid_level(level)) {
+        return BAD_FUNC_ARG;
+    }
+    key->level = level;
+    return 0;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_GetLevel(PQCleanHqcKey *key, int *level) {
+    if ((NULL == key) || (NULL == level)) {
+        return BAD_FUNC_ARG;
+    }
+    if (!is_valid_level(key->level)) {
+        return BAD_FUNC_ARG;
+    }
+    *level = key->level;
+    return 0;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_MakeKey(PQCleanHqcKey *key, WC_RNG *rng) {
+    int ret = 0;
+
+    if ((NULL == key) || (NULL == rng))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+
+    switch (key->level) {
+    case 1:
+        ret = PQCLEAN_HQC128_CLEAN_crypto_kem_keypair(key->pubKey, key->privKey, rng);
+        break;
+    case 3:
+        ret = PQCLEAN_HQC192_CLEAN_crypto_kem_keypair(key->pubKey, key->privKey, rng);
+        break;
+    case 5:
+        ret = PQCLEAN_HQC256_CLEAN_crypto_kem_keypair(key->pubKey, key->privKey, rng);
+        break;
+        /* level already validated */
+    }
+    if (ret == 0) {
+        key->pubKeySet = 1;
+        key->privKeySet = 1;
+        return ret;
+    }
+    return WC_FAILURE;
+}
+
+/* HQC seeds are not straightforwardly exposed */
+WOLFSSL_API int wc_PQCleanHqcKey_MakeKeyWithRandom(PQCleanHqcKey *key, const byte *rand, int len) {
+    return NOT_COMPILED_IN;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_CipherTextSize(PQCleanHqcKey *key, word32 *len) {
+    if ((NULL == key) || (NULL == len))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    switch (key->level) {
+    case 1:
+        *len = PQCLEAN_HQC_LEVEL1_CIPHERTEXT_SIZE;
+        break;
+    case 3:
+        *len = PQCLEAN_HQC_LEVEL3_CIPHERTEXT_SIZE;
+        break;
+    case 5:
+        *len = PQCLEAN_HQC_LEVEL5_CIPHERTEXT_SIZE;
+        break;
+    }
+    return 0;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_SharedSecretSize(PQCleanHqcKey *key, word32 *len) {
+    if ((NULL == key) || (NULL == len))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    switch (key->level) {
+    case 1:
+        *len = PQCLEAN_HQC_LEVEL1_SHAREDSECRET_SIZE;
+        break;
+    case 3:
+        *len = PQCLEAN_HQC_LEVEL3_SHAREDSECRET_SIZE;
+        break;
+    case 5:
+        *len = PQCLEAN_HQC_LEVEL5_SHAREDSECRET_SIZE;
+        break;
+    }
+    return 0;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_Encapsulate(PQCleanHqcKey *key, byte *ct, byte *ss, WC_RNG *rng) {
+    int ret = 0;
+    if ((key == NULL) || (ct == NULL) || (ss == NULL) || (rng == NULL)) {
+        return BAD_FUNC_ARG;
+    }
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    if (!key->pubKeySet) {
+        return MISSING_KEY;
+    }
+    switch (key->level) {
+    case 1:
+        ret = PQCLEAN_HQC128_CLEAN_crypto_kem_enc(ct, ss, key->pubKey, rng);
+        break;
+    case 3:
+        ret = PQCLEAN_HQC192_CLEAN_crypto_kem_enc(ct, ss, key->pubKey, rng);
+        break;
+    case 5:
+        ret = PQCLEAN_HQC256_CLEAN_crypto_kem_enc(ct, ss, key->pubKey, rng);
+        break;
+        /* level already validated */
+    }
+    return (ret == 0) ? 0 : BAD_FUNC_ARG;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_EncapsulateWithRandom(PQCleanHqcKey *key, byte *ct, byte *ss,
+                                                       const byte *rand, int len) {
+    return NOT_COMPILED_IN;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_Decapsulate(PQCleanHqcKey *key, byte *ss, const byte *ct,
+                                             word32 len) {
+    int ret = 0;
+    word32 ctLen = 0;
+
+    if ((key == NULL) || (ss == NULL) || (ct == NULL))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    if (!key->privKeySet)
+        return MISSING_KEY;
+    ret = wc_PQCleanHqcKey_CipherTextSize(key, &ctLen);
+    if (ret != 0) {
+        WOLFSSL_MSG("Failed to get HQC ciphertext size");
+        return BAD_FUNC_ARG;
+    }
+    if (len < ctLen) {
+        WOLFSSL_MSG_EX("Need %d bytes of ct buf, given %d", ctLen, len);
+        return BUFFER_E;
+    }
+
+    switch (key->level) {
+    case 1:
+        ret = PQCLEAN_HQC128_CLEAN_crypto_kem_dec(ss, ct, key->privKey);
+        break;
+    case 3:
+        ret = PQCLEAN_HQC192_CLEAN_crypto_kem_dec(ss, ct, key->privKey);
+        break;
+    case 5:
+        ret = PQCLEAN_HQC256_CLEAN_crypto_kem_dec(ss, ct, key->privKey);
+        break;
+        /* level already validated */
+    }
+
+    if (ret != 0)
+        ret = BAD_FUNC_ARG;
+    return ret;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_DecodePrivateKey(PQCleanHqcKey *key, const byte *in, word32 len) {
+    int ret = 0;
+    word32 privKeyLen = 0;
+    if ((key == NULL) || (in == NULL))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    if (key->privKeySet) {
+        WOLFSSL_MSG("HQC priv key already set");
+        return BAD_FUNC_ARG;
+    }
+    ret = wc_PQCleanHqcKey_PrivateKeySize(key, &privKeyLen);
+    if (ret != 0) {
+        WOLFSSL_MSG("Failed to get HQC private key size");
+        return BAD_FUNC_ARG;
+    }
+    if (privKeyLen != len) {
+        WOLFSSL_MSG_EX("Expect %d bytes, given %d bytes", privKeyLen, len);
+        return BUFFER_E;
+    }
+
+    XMEMCPY(key->privKey, in, len);
+    key->privKeySet = 1;
+
+    return ret;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_DecodePublicKey(PQCleanHqcKey *key, const byte *in, word32 len) {
+    int ret = 0;
+    word32 pubKeyLen = 0;
+    if ((key == NULL) || (in == NULL))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    if (key->pubKeySet) {
+        WOLFSSL_MSG("HQC pub key already set");
+        return BAD_FUNC_ARG;
+    }
+    ret = wc_PQCleanHqcKey_PublicKeySize(key, &pubKeyLen);
+    if (ret != 0) {
+        WOLFSSL_MSG("Failed to get HQC public key size");
+        return BAD_FUNC_ARG;
+    }
+    if (pubKeyLen != len) {
+        WOLFSSL_MSG_EX("Expect %d bytes, given %d bytes", pubKeyLen, len);
+        return BUFFER_E;
+    }
+
+    XMEMCPY(key->pubKey, in, len);
+    key->pubKeySet = 1;
+
+    return ret;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_PrivateKeySize(PQCleanHqcKey *key, word32 *len) {
+    if ((NULL == key) || (NULL == len))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    switch (key->level) {
+    case 1:
+        *len = PQCLEAN_HQC_LEVEL1_SECRETKEY_SIZE;
+        break;
+    case 3:
+        *len = PQCLEAN_HQC_LEVEL3_SECRETKEY_SIZE;
+        break;
+    case 5:
+        *len = PQCLEAN_HQC_LEVEL5_SECRETKEY_SIZE;
+        break;
+    }
+    return 0;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_PublicKeySize(PQCleanHqcKey *key, word32 *len) {
+    if ((NULL == key) || (NULL == len))
+        return BAD_FUNC_ARG;
+    if (!is_valid_level(key->level))
+        return BAD_FUNC_ARG;
+    switch (key->level) {
+    case 1:
+        *len = PQCLEAN_HQC_LEVEL1_PUBLICKEY_SIZE;
+        break;
+    case 3:
+        *len = PQCLEAN_HQC_LEVEL3_PUBLICKEY_SIZE;
+        break;
+    case 5:
+        *len = PQCLEAN_HQC_LEVEL5_PUBLICKEY_SIZE;
+        break;
+    }
+    return 0;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_EncodePrivateKey(PQCleanHqcKey *key, byte *out, word32 len) {
+    word32 privKeyLen = 0;
+    if ((key == NULL) || (out == NULL)) {
+        return BAD_FUNC_ARG;
+    }
+    if (!is_valid_level(key->level)) {
+        return BAD_FUNC_ARG;
+    }
+
+    if (wc_PQCleanHqcKey_PrivateKeySize(key, &privKeyLen) != 0) {
+        WOLFSSL_MSG("Failed to get HQC privKey size");
+        return BAD_FUNC_ARG;
+    }
+    if (len < privKeyLen) {
+        WOLFSSL_MSG_EX("Need %d bytes, given %d bytes", privKeyLen, len);
+        return BUFFER_E;
+    }
+    if (!key->privKeySet) {
+        return MISSING_KEY;
+    }
+    XMEMCPY(out, key->privKey, privKeyLen);
+
+    return 0;
+}
+
+WOLFSSL_API int wc_PQCleanHqcKey_EncodePublicKey(PQCleanHqcKey *key, byte *out, word32 len) {
+    word32 pubKeyLen = 0;
+    if ((key == NULL) || (out == NULL)) {
+        return BAD_FUNC_ARG;
+    }
+    if (!is_valid_level(key->level)) {
+        return BAD_FUNC_ARG;
+    }
+
+    if (wc_PQCleanHqcKey_PublicKeySize(key, &pubKeyLen) != 0) {
+        WOLFSSL_MSG("Failed to get HQC pubKey size");
+        return BAD_FUNC_ARG;
+    }
+    if (len < pubKeyLen) {
+        WOLFSSL_MSG_EX("Need %d bytes, given %d bytes", pubKeyLen, len);
+        return BUFFER_E;
+    }
+    if (!key->pubKeySet) {
+        return MISSING_KEY;
+    }
+    XMEMCPY(out, key->pubKey, pubKeyLen);
+
+    return 0;
+}
---
+++ wolfssl/wolfcrypt/pqclean_hqc.h
@@ -0,0 +1,76 @@
+/* A port of PQClean's `clean` implementation of HQC
+ */
+#ifndef PQCLEAN_HQC_H
+#define PQCLEAN_HQC_H
+
+#include <wolfssl/wolfcrypt/pqclean/crypto_kem/hqc-128/clean/api.h>
+#include <wolfssl/wolfcrypt/pqclean/crypto_kem/hqc-192/clean/api.h>
+#include <wolfssl/wolfcrypt/pqclean/crypto_kem/hqc-256/clean/api.h>
+#include <wolfssl/wolfcrypt/random.h>
+#include <wolfssl/wolfcrypt/types.h>
+
+#define PQCLEAN_HQC_LEVEL1_CIPHERTEXT_SIZE PQCLEAN_HQC128_CLEAN_CRYPTO_CIPHERTEXTBYTES
+#define PQCLEAN_HQC_LEVEL3_CIPHERTEXT_SIZE PQCLEAN_HQC192_CLEAN_CRYPTO_CIPHERTEXTBYTES
+#define PQCLEAN_HQC_LEVEL5_CIPHERTEXT_SIZE PQCLEAN_HQC256_CLEAN_CRYPTO_CIPHERTEXTBYTES
+
+#define PQCLEAN_HQC_LEVEL1_PUBLICKEY_SIZE PQCLEAN_HQC128_CLEAN_CRYPTO_PUBLICKEYBYTES
+#define PQCLEAN_HQC_LEVEL3_PUBLICKEY_SIZE PQCLEAN_HQC192_CLEAN_CRYPTO_PUBLICKEYBYTES
+#define PQCLEAN_HQC_LEVEL5_PUBLICKEY_SIZE PQCLEAN_HQC256_CLEAN_CRYPTO_PUBLICKEYBYTES
+
+#define PQCLEAN_HQC_LEVEL1_SECRETKEY_SIZE PQCLEAN_HQC128_CLEAN_CRYPTO_SECRETKEYBYTES
+#define PQCLEAN_HQC_LEVEL3_SECRETKEY_SIZE PQCLEAN_HQC192_CLEAN_CRYPTO_SECRETKEYBYTES
+#define PQCLEAN_HQC_LEVEL5_SECRETKEY_SIZE PQCLEAN_HQC256_CLEAN_CRYPTO_SECRETKEYBYTES
+
+#define PQCLEAN_HQC_LEVEL1_SHAREDSECRET_SIZE PQCLEAN_HQC128_CLEAN_CRYPTO_BYTES
+#define PQCLEAN_HQC_LEVEL3_SHAREDSECRET_SIZE PQCLEAN_HQC192_CLEAN_CRYPTO_BYTES
+#define PQCLEAN_HQC_LEVEL5_SHAREDSECRET_SIZE PQCLEAN_HQC256_CLEAN_CRYPTO_BYTES
+
+#define PQCLEAN_HQC_MAX_SECRETKEY_SIZE PQCLEAN_HQC_LEVEL5_SECRETKEY_SIZE
+#define PQCLEAN_HQC_MAX_PUBLICKEY_SIZE PQCLEAN_HQC_LEVEL5_PUBLICKEY_SIZE
+#define PQCLEAN_HQC_MAX_CIPHERTEXT_SIZE PQCLEAN_HQC_LEVEL5_CIPHERTEXT_SIZE
+#define PQCLEAN_HQC_MAX_SHAREDSECRET_SIZE PQCLEAN_HQC_LEVEL5_SHAREDSECRET_SIZE
+
+typedef struct PQCleanHqcKey {
+    /* 1, 3, or 5 */
+    int level;
+    byte privKey[PQCLEAN_HQC_MAX_SECRETKEY_SIZE];
+    byte pubKey[PQCLEAN_HQC_MAX_PUBLICKEY_SIZE];
+    /* 1 if secret key is set */
+    byte privKeySet;
+    /* 1 if public key is set */
+    byte pubKeySet;
+} PQCleanHqcKey;
+
+/* Public API, copied directly from wolfssl/wolfcrypt/mlkem.h */
+
+WOLFSSL_API int wc_PQCleanHqcKey_Init(PQCleanHqcKey *key);
+WOLFSSL_API int wc_PQCleanHqcKey_InitEx(PQCleanHqcKey *key, void *heap, int devId);
+WOLFSSL_API int wc_PQCleanHqcKey_Free(PQCleanHqcKey *key);
+
+WOLFSSL_API int wc_PQCleanHqcKey_SetLevel(PQCleanHqcKey *key, int level);
+WOLFSSL_API int wc_PQCleanHqcKey_GetLevel(PQCleanHqcKey *key, int *level);
+WOLFSSL_API int wc_PQCleanHqcKey_MakeKey(PQCleanHqcKey *key, WC_RNG *rng);
+WOLFSSL_API int wc_PQCleanHqcKey_MakeKeyWithRandom(PQCleanHqcKey *key, const byte *rand,
+                                                     int len);
+
+WOLFSSL_API int wc_PQCleanHqcKey_CipherTextSize(PQCleanHqcKey *key, word32 *len);
+WOLFSSL_API int wc_PQCleanHqcKey_SharedSecretSize(PQCleanHqcKey *key, word32 *len);
+
+WOLFSSL_API int wc_PQCleanHqcKey_Encapsulate(PQCleanHqcKey *key, byte *ct, byte *ss,
+                                               WC_RNG *rng);
+WOLFSSL_API int wc_PQCleanHqcKey_EncapsulateWithRandom(PQCleanHqcKey *key, byte *ct, byte *ss,
+                                                         const byte *rand, int len);
+WOLFSSL_API int wc_PQCleanHqcKey_Decapsulate(PQCleanHqcKey *key, byte *ss, const byte *ct,
+                                               word32 len);
+
+WOLFSSL_API int wc_PQCleanHqcKey_DecodePrivateKey(PQCleanHqcKey *key, const byte *in,
+                                                    word32 len);
+WOLFSSL_API int wc_PQCleanHqcKey_DecodePublicKey(PQCleanHqcKey *key, const byte *in,
+                                                   word32 len);
+
+WOLFSSL_API int wc_PQCleanHqcKey_PrivateKeySize(PQCleanHqcKey *key, word32 *len);
+WOLFSSL_API int wc_PQCleanHqcKey_PublicKeySize(PQCleanHqcKey *key, word32 *len);
+WOLFSSL_API int wc_PQCleanHqcKey_EncodePrivateKey(PQCleanHqcKey *key, byte *out, word32 len);
+WOLFSSL_API int wc_PQCleanHqcKey_EncodePublicKey(PQCleanHqcKey *key, byte *out, word32 len);
+
+#endif
